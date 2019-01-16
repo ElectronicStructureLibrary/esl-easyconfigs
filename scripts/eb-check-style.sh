b9aa398cd37c5bf96a8cc24d55ff45b7aa5766ce
Yann Pouillon <devops@materialsevolution.es>
2019-01-16 11:31:05 +0100

    [ci] Improved the output of the style checker script

--- scripts/eb-check-style.sh
+++ scripts/eb-check-style.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
+# Init script return code
 chk_ret=0
+
+# Check the style of all EasyConfigs
+chk_style_fails=""
 for eb_src in [0-9].[0-9].[0-9]/*; do
-  echo "===== Begin check style: ${eb_src} ====="
+  echo "EasyBuild - Checking style of ${eb_src}"
   eb --check-style "${eb_src}"
   eb_ret="${?}"
-  echo "===== End   check style: ${eb_src} ====="
+  echo ""
   test "${chk_ret}" = "0" && chk_ret="${eb_ret}"
+  test "${eb_ret}" != "0" && chk_style_fails="${chk_style_fails} ${eb_src}"
 done
+
+# Report banner
+echo ""
+echo "***** All tests finished *****"
+echo ""
+
+# Report style issues
+if test ! -z "${chk_style_fails}"; then
+  echo "The following EasyConfigs have style issues:"
+  echo ""
+  for chk_fail in ${chk_style_fails}; do
+    echo "  * ${chk_fail}"
+  done
+  echo ""
+fi
 
 exit "${chk_ret}"
